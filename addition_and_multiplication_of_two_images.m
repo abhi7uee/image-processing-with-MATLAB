37a00a181d2571978c48e40daee417a0e9093965
abhi7uee <72334671+abhi7uee@users.noreply.github.com>
2020-10-04 13:36:47 +0530

    addition_and_multiplication_of_two_images.m

---
+++ addition_and_multiplication_of_two_images.m
@@ -0,0 +1,18 @@
+Program to illustrate the addition and multiplication of two images.
+clear all;
+close all;
+i=imread('C:\Users\student.researchlab-HP\Desktop\DSIP-lab\imagesblack.jpg');
+j=imread('C:\Users\student.researchlab-HP\Desktop\DSIP-lab\image4.jpg');
+i1=imresize(i,[256,256]);
+j1=imresize(j,[256,256]);
+k=imadd(i1,j1);
+l=immultiply(i1,j1);
+subplot(2,2,1);
+imshow(i1), title('image1');
+subplot(2,2,2);
+imshow(j1), title('image 2');
+subplot(2,2,3);
+imshow(k), title('Addition of images');
+subplot(2,2,4);
+ imshow(l), title('multiplication of images');
+
